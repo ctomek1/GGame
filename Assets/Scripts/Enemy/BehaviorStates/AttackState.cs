d1c2b4b89f364516747e74268da33a931030af4d
Tomasz Ciechanowski <tomaszciechanowski@wenklystudio.com>
2021-12-13 21:33:33 +0100

    removed null exception possibility

--- Assets/Scripts/Enemy/BehaviorStates/AttackState.cs
+++ Assets/Scripts/Enemy/BehaviorStates/AttackState.cs
@@ -13,7 +13,6 @@ namespace StateMachine
 
         public override Type Tick()
         {
-            Debug.LogError(Target);
             ChaseState chaseState = GetComponent<ChaseState>();
             Target = chaseState.Target;
 
@@ -21,12 +20,17 @@ namespace StateMachine
             {
                 enemy.gameObject.transform.parent.LookAt(new Vector3(Target.transform.position.x, transform.position.y, Target.transform.position.z));
                 weaponView.Fire(Target.transform.position);
-            }     
-            
-            if(Target == null || Target.GetComponent<Health>().HpPoints == 0)
+            }
+            else
             {
                 return typeof(IddleState);
             }
+
+            if(Target.GetComponent<Health>().HpPoints == 0)
+            {
+                return typeof(IddleState);
+            }
+
             return null;
         }
     }
