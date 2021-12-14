9681a9fb283e33e2d8087359d6de71c8c96423ed
Tomasz Ciechanowski <tomaszciechanowski@wenklystudio.com>
2021-12-14 22:02:47 +0100

    player view with death condition

--- Assets/Scripts/Views/PlayerView.cs
+++ Assets/Scripts/Views/PlayerView.cs
@@ -8,25 +8,46 @@ namespace Views
     {
         public static event Action<PlayerView, Vector3> OnPlayerFired;
 
-        [SerializeField] PlayerModel playerModel;
+        [SerializeField] KillableCharacterModel playerModel;
         [SerializeField] Camera cam;
         [SerializeField] Animator anim;
         [SerializeField] PlayerMovement playerMovement;
+        [SerializeField] Health.Health health;
         private static readonly int DIE_TRIGGER = Animator.StringToHash("Die");
+        private bool isPlayerAlive = true;
+
+        private int enemiesKilled;
+
+        public bool IsPlayerAlive { get => isPlayerAlive; }
+        public Health.Health Health { get => health;}
+        public int EnemiesKilled { get => enemiesKilled; set => enemiesKilled = value; }
+
+        private void Awake()
+        {
+            health.HpPoints = playerModel.HealthPoints;
+        }
+
+        private void OnEnable()
+        {
+            health.OnCharacterDead += PlayerDied;
+        }
 
         void Update()
         {
-            playerMovement.HandleMovement();
-            if (Input.GetMouseButton(0))
+            if(IsPlayerAlive)
             {
-                var destination = GetFireDestination();
-                OnPlayerFired?.Invoke(this, destination);
-            }
+                playerMovement.HandleMovement();
+                if (Input.GetMouseButton(0))
+                {
+                    var destination = GetFireDestination();
+                    OnPlayerFired?.Invoke(this, destination);
+                }
+            }           
         }
 
         Vector3 GetFireDestination()
         {
-            var ray = cam.ScreenPointToRay(
+            var ray = cam.ViewportPointToRay(
                 new Vector3(Input.mousePosition.x,
                     Input.mousePosition.y,
                     Input.mousePosition.z));
@@ -36,6 +57,12 @@ namespace Views
         public void Die()
         {
             anim.SetTrigger(DIE_TRIGGER);
+            PlayerDied();
+        }
+
+        private void PlayerDied()
+        {
+            isPlayerAlive = false;
         }
     }
 }
