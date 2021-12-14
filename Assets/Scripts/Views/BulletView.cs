a4189e3f1be488910a0e976011761f34c5963875
Tomasz Ciechanowski <tomaszciechanowski@wenklystudio.com>
2021-12-14 01:41:41 +0100

    added bullet dmg from model

--- Assets/Scripts/Views/BulletView.cs
+++ Assets/Scripts/Views/BulletView.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
+using Health;
+using Models;
 
 namespace Views
 {
     public class BulletView : MonoBehaviour
     {
         [SerializeField] float autoDestroyAfterSeconds = 4f;
+        [SerializeField] BulletDamageModel bulletDamage;
 
         void Start()
         {
-            Destroy(gameObject, autoDestroyAfterSeconds);
+            var damage = GetComponent<DamageDealer>();
+            damage.Damage = bulletDamage.Damage;
+            Destroy(gameObject, autoDestroyAfterSeconds);            
         }
     }
 }
