ac1e537e316c94077a9dcbaec3da778e10788f45
Tomasz Ciechanowski <tomaszciechanowski@wenklystudio.com>
2021-12-15 01:28:26 +0100

    remove useless declaration

--- Assets/Scripts/Models/WeaponModel.cs
+++ Assets/Scripts/Models/WeaponModel.cs
@@ -8,13 +8,11 @@ namespace Models
         [SerializeField] float fireCooldown = 1f;
         [SerializeField] float projectileSpeed = 10;
         [SerializeField] GameObject projectilePrefab;
-        [SerializeField] float reloadTime = 3f;
         [SerializeField] float bulletsInMagazine = 10;
         
         public float FireCooldown => fireCooldown;
         public float ProjectileSpeed => projectileSpeed;
         public GameObject ProjectilePrefab => projectilePrefab;
-        public float ReloadTime { get => reloadTime; set => reloadTime = value; }
         public float BulletsInMagazine { get => bulletsInMagazine; set => bulletsInMagazine = value; }
     }
 }
